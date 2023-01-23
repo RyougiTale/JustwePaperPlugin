e6cf7f1115acacc8fa731bab632be810a72a951b
RyougiTale <tianlonghuangwu@gmail.com>
2023-01-23 13:28:44 +0800

    first commit

---
+++ src/main/java/io/papermc/testplugin/ExamplePlugin.java
@@ -0,0 +1,27 @@
+package io.papermc.testplugin;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ExamplePlugin extends JavaPlugin implements Listener {
+    @Override
+    public void onDisable() {
+        System.out.println("onDisable");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("onEnable");
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    }
+
+}
